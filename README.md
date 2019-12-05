ee8dc240ad031aea10af196ce3984b0b3b1c24fb
ahmed <ahmed.essam1799@gmail.com>
2019-12-05 03:38:31 +0200

    Building History & Responsive home

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-## SOON
+[YouTube Data API](https://developers.google.com/youtube/v3)
+
+## Skills used:
+- [jQuery](https://github.com/jquery/jquery)
+- Ajax
+
+## Plugins/Tools used:
+- [Bootstrap-Grid](https://github.com/twbs/bootstrap)
+- [Moment](https://github.com/moment/moment/)
+- [Fontawesome](https://github.com/FortAwesome/react-fontawesome)
+- [normalize.css](https://github.com/necolas/normalize.css)
--- dist/js/youtubeAPi.js
+++ dist/js/youtubeAPi.js
@@ -4,11 +4,14 @@ $(function () {
       default_url: 'https://www.googleapis.com/youtube/v3',
       api_key: 'AIzaSyA4ChC9gzj1gOdHUTkCmxfbTD4SuNQr8iM',
       max_results: 50
-   }
+   },
+   history = JSON.parse(localStorage.getItem('history')),
+   goToHistory = Array.isArray(history) ? history : [];
 
    function errorsToggle(e) {
       if (e === 'show') {
          $('.errors').fadeIn(250)
+
       } else {
          $('.errors').fadeOut(250)
       }
@@ -20,11 +23,11 @@ $(function () {
       var value = $('input[name="search"]').val().trim();
 
       if (value) {
-
          valuefilter = '';
          
          if (value.lastIndexOf('/') + 1 === value.length) {
             valuefilter = value.slice(value.lastIndexOf('UC'), value.length - 1)
+
          } else {
             valuefilter = value.slice(value.lastIndexOf('UC'), value.length)
          }
@@ -37,12 +40,23 @@ $(function () {
                url: `
                   ${st.default_url}/search?part=snippet&channelId=${valuefilter}&maxResults=${st.max_results}&key=${st.api_key}
                `,
-               success: function(data, xhr, status) {
-                  console.log(data)
+               success: function(data) {
+                  localStorage.setItem('info_channel', JSON.stringify(data))
+
+                  goToHistory.push({
+                     id: data.items[0].snippet.channelId, 
+                     name: data.items[0].snippet.channelTitle,
+                     date: new Date()
+                  })
+
+                  localStorage.setItem('history', JSON.stringify(goToHistory))
+               },
+               complete: function(xhr, status) {
                   console.log(xhr)
                   console.log(status)
                }
             })
+            
          } else {
             errorsToggle('show')
          }
@@ -50,4 +64,25 @@ $(function () {
          errorsToggle('show')
       }
    })
+
+   if (Array.isArray(history)) {
+      $('.history .empty').css('display', 'none')
+      $('#history').css('display', 'block')
+
+      history.forEach(function(item) {
+         $('#history').prepend(`
+            <div>
+               <div class="who">
+                  <span>${item.name}</span>
+                  <span>${item.id}</span>
+               </div>
+               <span>${moment(item.date).fromNow()}</span>
+            </div>
+         `)
+      })
+
+   } else {
+      $('#history').css('display', 'none')
+      $('.history .empty').css('display', 'block')
+   }
 })
